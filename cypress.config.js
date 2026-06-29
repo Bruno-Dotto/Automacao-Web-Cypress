2fef3261621c7cc0bf9c688793363a51d49619fc
Bruno-Dotto <brunodtt@gmail.com>
2026-06-28 22:43:07 -0300

    Ajuste no cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,3 @@
-// const { defineConfig } = require("cypress");
-// const grep = require('@cypress/grep/src/plugin');
-
-// module.exports = defineConfig({
-//   allowCypressEnv: false,
-
-//   e2e: {
-//     baseUrl: 'https://automationpratice.com.br',
-//     defaultCommandTimeout: 10000,
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//         grep(config);
-
-//       return config;
-//     },
-//   },
-// });
 
 const { defineConfig } = require("cypress");
 const { plugin: cypressGrepPlugin } = require("@cypress/grep/plugin");
